1433ef1866bbcaaa9e04c4b94bc38875b7bfebc0
vjta088x <growtopian52@gmail.com>
2023-03-09 10:49:33 +0100

    add

--- asm-module.asm
+++ asm-module.asm
@@ -11,15 +11,24 @@
 
     section .data
     extern mocnina
+
     
+    extern array_length;
+    extern pole1
+    extern pole2
+    extern result_pole
+
 g_i dd 0
 result dd 0
+
+pole_i dd 0
     
 
 ;***************************************************************************
 
     section .text
     global is_devidable
+    global count_pole
 is_devidable:
     cmp dword[g_i], 32 ;g_i = 0
     jg .calc_result
@@ -52,4 +61,18 @@ is_devidable:
     ret
     ;ret
 
+count_pole:
+    mov eax, dword[pole_i]
+    cmp eax, [array_length]
+    je .return
 
+    mov ecx, dword [pole_i]
+    mov edx, dword[pole1 + ecx *4]
+    mov ebx, dword [pole2 + ecx *4]
+    add edx, ebx
+    mov dword [result_pole + ecx *4], edx
+
+    inc dword [pole_i]
+    jmp count_pole
+.return: 
+    ret
--- c-main.c
+++ c-main.c
@@ -16,6 +16,15 @@
 // type g_some_c_variable;
 int mocnina = 33;
 
+int array_length = 6;
+
+int pole1[] = {0,1,2,3,4,5};
+int pole2[] = {5,4,3,2,1,0};
+
+int result_pole[] = {0,0,0,0,0,0};
+
+void count_pole();
+
 bool is_devidable();
 
 int main()
@@ -25,4 +34,12 @@ int main()
         printf("Je mocninou \n");
     else
         printf("Neni mocninou \n");
+
+    count_pole();
+
+    for(int i = 0; i < array_length; i ++){
+        printf("%d ", result_pole[i]);
+    }
+    printf("\n");
+
 }
